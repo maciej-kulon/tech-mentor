edd015483caf0fd1a5a64c063fb6c22384bd564e
Maciej Kulon <maciejkulon1@gmail.com>
2025-04-20 17:49:41 +0200

    feat: add jwt auth (#23)
    added auth module
    added get user EP
    added login EP
    
    Co-authored-by: Maciej Kulon <maciej.kulon@flip.shop>

--- backend/src/user/user.repository.ts
+++ backend/src/user/user.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 import { IUser } from './interfaces/IUser';
 import { User } from './user.schema';
@@ -16,10 +16,27 @@ export class UserRepository {
       name: createdUser.name,
       surname: createdUser.surname,
       email: createdUser.email,
+      createdAt: createdUser.createdAt,
+      updatedAt: createdUser.updatedAt,
     };
   }
 
   public async findByEmail(email: string): Promise<User | null> {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  public async findById(id: string): Promise<IUser | null> {
+    const user = await this.userModel.findOne({ _id: new Types.ObjectId(id) }).exec();
+    if (!user) {
+      return null;
+    }
+    return {
+      id: user._id.toString(),
+      name: user.name,
+      surname: user.surname,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
   }
 }
